cd20831b16d5b83b3cd4df3b153713839ffea086
Daniel Douek <daniel@swimm.io>
2021-12-09 13:58:30 +0200

    set everything to be ready to swimminar include commenting

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ function add(a, b) {
 	return a + b;
 }
 
+let a =100;
+
 function helloWorld() {
 	let a = 1;
 	let b = 2;
@@ -15,7 +17,7 @@ function helloWorld() {
 	console.log(add(a, c));
 	console.log('This is a string')
 	console.log(add(b, b));
-    const result = add(7, 6);
+  const result = add(7, 6);
 }
 
 const result = add(1, 2);
@@ -27,9 +29,6 @@ const codeLines = code.split("\n");
 
 const printLines = (start, end) => {
   console.log(codeLines.slice(start - 1, end).join("\n"));
-  //   for (let i = start - 1; i <= end; i++) {
-  //     console.log(codeLines[i]);
-  //   }
 };
 
 // console.log("--------------------------------------------------");
@@ -42,6 +41,7 @@ const printLines = (start, end) => {
 //       value: token.value,
 //       start: token.start,
 //       end: token.end,
+//       type: token.type.label,
 //     }))
 //     .filter(({ value }) => !!value) // like comments and withespaces and parenthesis
 // );
@@ -56,12 +56,12 @@ const printLines = (start, end) => {
 //     ecmaVersion: "2022",
 //   }),
 //   (node) => {
-//     if (node.type === "FunctionDeclaration") {
-//       console.log(node);
-//     }
-//     if (node.type === "CallExpression") {
-//       console.log(node);
-//     }
+//     // if (node.type === "FunctionDeclaration") {
+//     //   console.log(node);
+//     // }
+//     // if (node.type === "CallExpression") {
+//     //   console.log(node);
+//     // }
 //     console.log(node);
 //   }
 // );
@@ -79,116 +79,87 @@ const printLines = (start, end) => {
 //     if (
 //       node.type === "FunctionDeclaration" &&
 //       node.id &&
-//       node.id.name === searchFor
+//       node.id.name === searchFor // add
 //     ) {
 //       console.log(node);
+//       console.log(`--------- \n`);
 //       printLines(node.loc.start.line, node.loc.end.line);
 //     }
 //   }
 // );
 
-console.log("--------------------------------------------------");
-console.log("The Function Calls Expressions");
-console.log("--------------------------------------------------");
+// console.log("--------------------------------------------------");
+// console.log("The Function Calls Expressions");
+// console.log("--------------------------------------------------");
 
-const nodes = [];
+// const nodes = [];
 
-walk.full(
-  acorn.parse(code, {
-    sourceType: "module",
-    locations: true,
-    ecmaVersion: "2022",
-  }),
-  (node) => {
-    if (node.type === "CallExpression") {
-      if (node.callee.name === searchFor) {
-        // console.log(`Node: ${JSON.stringify(node)} \n`);
-        // printLines(node.loc.start.line, node.loc.end.line);
-        // console.log("\n");
-        const nodeArguments = node.arguments
-          .filter((arg) => arg.type === "Identifier" || arg.type === "Literal")
-          .map((arg) => ({
-            name: arg.name,
-            value: arg.value,
-            location: arg.loc.start.line - 1,
-          }));
-        nodes.push({
-          node: node.callee,
-          args: nodeArguments,
-        });
-      }
-    }
-  }
-);
+// walk.full(
+//   acorn.parse(code, {
+//     sourceType: "module",
+//     locations: true,
+//     ecmaVersion: "2022",
+//   }),
+//   (node) => {
+//     if (node.type === "CallExpression" && node.callee.name === searchFor) {
+//       console.log(`Node: ${JSON.stringify(node)} \n`);
+//       printLines(node.loc.start.line, node.loc.end.line);
+//       console.log("\n");
+//       // const nodeArguments = node.arguments
+//       //   .filter((arg) => arg.type === "Identifier" || arg.type === "Literal")
+//       //   .map((arg) => ({
+//       //     name: arg.name,
+//       //     value: arg.value,
+//       //     location: arg.loc.start.line - 1,
+//       //   }));
+//       // nodes.push({
+//       //   node: node.callee,
+//       //   args: nodeArguments,
+//       // });
+//     }
+//   }
+// );
 
-console.log([...nodes].map((node) => node.args));
+// console.log([...nodes].map((node) => node.args));
 
-console.log("--------------------------------------------------");
-console.log("Function Argument decleartion");
-console.log("--------------------------------------------------");
+// console.log("--------------------------------------------------");
+// console.log("Function Argument decleartion");
+// console.log("--------------------------------------------------");
 
-const withinBlockRange = (s, e, n) => {
-  return s <= n && n <= e;
-};
-const fullCodeTree = acorn.parse(code, { locations: true, ecmaVersion: 2022 });
+// const withinBlockRange = (start, end, node) => {
+//   return start <= node && node <= end;
+// };
+// const fullCodeTree = acorn.parse(code, { locations: true, ecmaVersion: 2022 });
 
-nodes.forEach((node) =>
-  node.args
-    .filter((arg) => arg.name)
-    .forEach((arg) => {
-      fullCodeTree.body.forEach((block) => {
-        // search the variable declarations for non literal arguments
-        if (
-          withinBlockRange(
-            block.loc.start.line,
-            block.loc.end.line,
-            node.node.loc.start.line
-          )
-        ) {
-          walk.simple(block, {
-            VariableDeclaration(node) {
-              if (node.declarations[0].id.name === arg.name) {
-                // the node is the declaration of the variable
-                arg.startLocation = node.loc.start.line;
-              }
-            },
-          });
-        }
-      });
-    })
-);
+// nodes.forEach((node) =>
+//   node.args
+//     .filter((arg) => arg.name)
+//     .forEach((arg) => {
+//       fullCodeTree.body.forEach((block) => {
+//         // search the variable declarations for non literal arguments
+//         if (
+//           withinBlockRange(
+//             block.loc.start.line,
+//             block.loc.end.line,
+//             node.node.loc.start.line
+//           )
+//         ) {
+//           walk.simple(block, {
+//             VariableDeclaration(node) {
+//               if (node.declarations[0].id.name === arg.name) {
+//                 // the node is the declaration of the variable
+//                 arg.startLocation = node.loc.start.line;
+//               }
+//             },
+//           });
+//         }
+//       });
+//     })
+// );
 
-const interestingLines = [];
+// const interestingLines = [];
 
-nodes.forEach((node) => {
-  node.args.forEach((arg) => {
-    if (arg.startLocation) {
-      interestingLines.push({
-        line: arg.startLocation,
-        data: codeLines.slice(arg.startLocation - 1, arg.startLocation),
-      });
-    }
-  });
-});
-
-function sortLines(a, b) {
-  if (a.line < b.line) {
-    return -1;
-  }
-  return 1;
-}
-const linesToPrint = Array.from([...new Set(interestingLines)]).sort(sortLines);
-console.log(linesToPrint);
-
-// no in use
 // nodes.forEach((node) => {
-//   interestingLines.push({
-//     line: node.node.loc.start.line,
-//     data: codeLines.slice(
-//       node.node.loc.start.line - 1,
-//       node.node.loc.start.line
-//     ),
-//   });
 //   node.args.forEach((arg) => {
 //     if (arg.startLocation) {
 //       interestingLines.push({
@@ -198,3 +169,13 @@ console.log(linesToPrint);
 //     }
 //   });
 // });
+
+// function sortLines(a, b) {
+//   if (a.line < b.line) {
+//     return -1;
+//   }
+//   return 1;
+// }
+// const linesToPrint = Array.from([...new Set(interestingLines)]).sort(sortLines);
+// console.log(linesToPrint);
+
